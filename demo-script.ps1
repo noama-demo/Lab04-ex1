82e3eb55c56bd8b8a1f7d17e7fd629254fc3222a
Leon Jalfon <leonj@sela.co.il>
2018-05-22 10:14:17 +0000

    Update demo-script.ps1

--- demo-script.ps1
+++ demo-script.ps1
@@ -9,5 +9,5 @@ function Create-GreenMessage
     Write-Host $Message -ForegroundColor Green
 }
 
-Create-GreenMessage -Message "My Message"
-Create-GreenMessage -Message "Master Message"
+Create-GreenMessage -Message "My master Message"
+Create-GreenMessage -Message "Master Message!"
